42af8984b7deb6ed229d735857a8e32b7380ebda
mariahgloehr <loehrm@carleton.edu>
2025-06-13 10:16:57 -0400

    full rf classifier tuning

--- RF/RF_classifier.py
+++ RF/RF_classifier.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 from sklearn.model_selection import GridSearchCV
 
 # Load data
-df = pd.read_csv("cell_cycle_tidied.csv")
+df = pd.read_csv("Data/cell_cycle_tidied.csv")
 
 # Combine phase M and G2 into one class
 df['phase'] = df['phase'].replace({'M': 'G2'})
@@ -18,7 +18,7 @@ y = df['phase']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=949, stratify=y)
 
 # Train Random Forest model with 500 trees and large depth
-rf = RandomForestClassifier(n_estimators=500, max_depth=50, max_features='sqrt', random_state=949)
+rf = RandomForestClassifier(n_estimators=600, max_depth=50, max_features='sqrt', random_state=949)
 rf.fit(X_train, y_train)
 
 # Predict on training and test sets
@@ -26,35 +26,31 @@ y_train_pred = rf.predict(X_train)
 y_test_pred = rf.predict(X_test)
 
 # Output results
-#print("=== Training Set ===")
-#print("Overall Accuracy:", accuracy_score(y_train, y_train_pred))
-#print("Per-Class Accuracy:\n", classification_report(y_train, y_train_pred, digits=3))
+print("=== Training Set ===")
+print("Overall Accuracy:", accuracy_score(y_train, y_train_pred))
 
-#print("\n=== Test Set ===")
-#print("Overall Accuracy:", accuracy_score(y_test, y_test_pred))
-#print("Per-Class Accuracy:\n", classification_report(y_test, y_test_pred, digits=3))
+print("\n=== Test Set ===")
+print("Overall Accuracy:", accuracy_score(y_test, y_test_pred))
 
 # For training set
-#df_train = pd.DataFrame({'true': y_train, 'pred': y_train_pred})
-#accuracy_per_phase_train = df_train.groupby('true').apply(lambda x: accuracy_score(x['true'], x['pred']))
+df_train = pd.DataFrame({'true': y_train, 'pred': y_train_pred})
+accuracy_per_phase_train = df_train.groupby('true').apply(lambda x: accuracy_score(x['true'], x['pred']))
 
-#print("Accuracy per phase (Train):")
-#print(accuracy_per_phase_train)
+print("Accuracy per phase (Train):")
+print(accuracy_per_phase_train)
 
 # For test set
-#df_test = pd.DataFrame({'true': y_test, 'pred': y_test_pred})
-#accuracy_per_phase_test = df_test.groupby('true').apply(lambda x: accuracy_score(x['true'], x['pred']))
+df_test = pd.DataFrame({'true': y_test, 'pred': y_test_pred})
+accuracy_per_phase_test = df_test.groupby('true').apply(lambda x: accuracy_score(x['true'], x['pred']))
 
-#print("\nAccuracy per phase (Test):")
-#print(accuracy_per_phase_test)
+print("\nAccuracy per phase (Test):")
+print(accuracy_per_phase_test)
 
-## cross-validation:
+## hypertuning
 
 #param_grid = {
-#    'n_estimators': [500, 600],              
-#    'max_depth': [50, 60]    
-#    #'max_features': ['sqrt', 'log2']   
-#    #'min_samples_split': [2, 5],        
+#    'n_estimators': [500, 600, 700],              
+#    'max_depth': [50, 60, 70]          
 #}
 
 # Set up GridSearch with 10-fold cross-validation optimizing for accuracy
@@ -75,11 +71,11 @@ y_test_pred = rf.predict(X_test)
 
 # kappa
 
-#kappa_train = cohen_kappa_score(y_train, y_train_pred)
-#kappa_test = cohen_kappa_score(y_test, y_test_pred)
+kappa_train = cohen_kappa_score(y_train, y_train_pred)
+kappa_test = cohen_kappa_score(y_test, y_test_pred)
 
-#print(f"Cohen's Kappa (Train): {kappa_train:.3f}")
-#print(f"Cohen's Kappa (Test): {kappa_test:.3f}")
+print(f"Cohen's Kappa (Train): {kappa_train:.3f}")
+print(f"Cohen's Kappa (Test): {kappa_test:.3f}")
 
 ## CONFUSION MATRIX
 print("Train Confusion Matrix")
--- RF/RF_regression.py
+++ RF/RF_regression.py
@@ -3,9 +3,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, root_mean_squared_error
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 
 # Load data
-df = pd.read_csv("cell_cycle_tidied.csv")
+df = pd.read_csv("Data/cell_cycle_tidied.csv")
 
 #df['phase'] = df['phase'].replace({'M': 'G2'})
 
@@ -30,41 +31,64 @@ rmse_test = root_mean_squared_error(y_test, y_test_pred)
 
 
 # Calculate R^2
-r2_train = r2_score(y_train, y_train_pred)
-r2_test = r2_score(y_test, y_test_pred)
+#r2_train = r2_score(y_train, y_train_pred)
+#r2_test = r2_score(y_test, y_test_pred)
 
-print("=== Training Set ===")
-print(f"RMSE: {rmse_train:.4f}")
-print(f"R^2: {r2_train:.4f}")
+#print("=== Training Set ===")
+#print(f"RMSE: {rmse_train:.4f}")
+#print(f"R^2: {r2_train:.4f}")
 
-print("\n=== Test Set ===")
-print(f"RMSE: {rmse_test:.4f}")
-print(f"R^2: {r2_test:.4f}")
+#print("\n=== Test Set ===")
+#print(f"RMSE: {rmse_test:.4f}")
+#print(f"R^2: {r2_test:.4f}")
 
 # For training set
-df_train = pd.DataFrame({
-    'true_age': y_train,
-    'pred_age': y_train_pred,
-    'phase': df.loc[y_train.index, 'phase']  # get phase for train samples
-})
+#df_train = pd.DataFrame({
+#    'true_age': y_train,
+#    'pred_age': y_train_pred,
+#    'phase': df.loc[y_train.index, 'phase']  # get phase for train samples
+#})
 
-rmse_per_phase_train = df_train.groupby('phase').apply(
-    lambda x: np.sqrt(mean_squared_error(x['true_age'], x['pred_age']))
-)
+#rmse_per_phase_train = df_train.groupby('phase').apply(
+#    lambda x: np.sqrt(mean_squared_error(x['true_age'], x['pred_age']))
+#)
 
-print("RMSE per phase (Train):")
-print(rmse_per_phase_train)
+#print("RMSE per phase (Train):")
+#print(rmse_per_phase_train)
 
 # For test set
-df_test = pd.DataFrame({
-    'true_age': y_test,
-    'pred_age': y_test_pred,
-    'phase': df.loc[y_test.index, 'phase']  # get phase for test samples
-})
+#df_test = pd.DataFrame({
+#    'true_age': y_test,
+#    'pred_age': y_test_pred,
+#    'phase': df.loc[y_test.index, 'phase']  # get phase for test samples
+#})
 
-rmse_per_phase_test = df_test.groupby('phase').apply(
-    lambda x: np.sqrt(mean_squared_error(x['true_age'], x['pred_age']))
+#rmse_per_phase_test = df_test.groupby('phase').apply(
+#    lambda x: np.sqrt(mean_squared_error(x['true_age'], x['pred_age']))
+#)
+
+#print("\nRMSE per phase (Test):")
+#print(rmse_per_phase_test)
+
+## hypertuning
+
+param_grid = {
+    'n_estimators': [500, 600, 700],              
+    'max_depth': [50, 60, 70]          
+}
+
+# Set up GridSearch with 10-fold cross-validation optimizing for accuracy
+grid_search = GridSearchCV(
+    estimator=rf,
+    param_grid=param_grid,
+    cv=10,
+    scoring='accuracy',
+    n_jobs=-1
 )
 
-print("\nRMSE per phase (Test):")
-print(rmse_per_phase_test)
+# Fit the model to your data
+grid_search.fit(X, y)
+
+#Output best settings and best accuracy
+print("Best parameters:", grid_search.best_params_)
+print("Best cross-validation accuracy:", grid_search.best_score_)
